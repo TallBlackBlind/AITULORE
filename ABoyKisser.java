09fc2dccd8a0812cc57fb648c43a8431fdd2b216
AmadeoMartell <medeu207@gmail.com>
2024-02-13 13:09:09 +0600

    Deleted garbage files

--- ABoyKisser.java
+++
@@ -1,7 +0,0 @@
-public class ABoyKisser {
-    public static void main(String[] args) {
-        for (int i = 0; i < 6; i++) {
-            System.out.printf("I've kissed %d femboys\n", i);
-        }
-    }
-}
--- Lament.java
+++
@@ -1,5 +0,0 @@
-public class Lament {
-    public static void main(String[] args) {
-        System.out.println("Fuck you mean lament");
-    }
-}
