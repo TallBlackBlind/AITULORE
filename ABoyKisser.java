87b43b643ff225156070c998a21ddce20284a7eb
Milterdone <alchemistgomunculus@gmail.com>
2024-02-13 12:56:32 +0600

    LAMENT first commit

---
+++ ABoyKisser.java
@@ -0,0 +1,7 @@
+public class ABoyKisser {
+    public static void main(String[] args) {
+        for (int i = 0; i < 6; i++) {
+            System.out.printf("I've kissed %d femboys\n", i);
+        }
+    }
+}
